b25e2c7544eadecba4b9eefeae18c90c6371fc0a
aDrIaN2367 <93383013+aDrIaN2367@users.noreply.github.com>
2021-12-14 23:05:41 +0100

    Create hija.java

---
+++ Cuestión 2/hija.java
@@ -0,0 +1,14 @@
+package cuestión2;
+
+public class hija extends Padre{
+	
+	
+	public hija(String mensaje, int numero) {
+		super(mensaje, numero);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void saludar() {
+		System.out.println("Hola soy la hija.");
+	}
+}
