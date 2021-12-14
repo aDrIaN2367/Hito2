b36b11fe4893af2cd7fb01693935d48a649a1f50
aDrIaN2367 <93383013+aDrIaN2367@users.noreply.github.com>
2021-12-14 23:07:15 +0100

    Create Gato.java

---
+++ Cuestión 3/Gato.java
@@ -0,0 +1,10 @@
+package cuestión3;
+
+public class Gato extends Animal{
+
+	@Override
+	public void sonido() {
+		// TODO Auto-generated method stub
+		System.out.println("El gato hace Meow");
+	}
+}
