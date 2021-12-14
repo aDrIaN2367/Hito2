43648df40df385147b2c14922a7a0df4b7483604
aDrIaN2367 <93383013+aDrIaN2367@users.noreply.github.com>
2021-12-14 23:04:26 +0100

    Create Principal.java

---
+++ Cuestión 1/Principal.java
@@ -0,0 +1,10 @@
+package cuestión1;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Trabajador trabajador=new Trabajador("Juan", "Madrid",30000f, false);
+		System.out.println(trabajador);
+	}
+	
+}
